96c160b8232603227e041de86a4d64ff8c3a5311
yunfeng-net <yunfeng.net@hotmail.com>
2018-08-30 20:00:15 +0800

    result correct

--- basic.py
+++ basic.py
@@ -25,32 +25,42 @@ class Agent(object):
     def update(self, observation, action, observation2, reward, done):
         state = self.env.state_id(observation)
         new_state = self.env.state_id(observation2)
+        f = self.Q[state, action]
         self.Q[state, action] = (1 - self.ALPHA) * self.Q[state, action] + \
             self.ALPHA * (reward + self.GAMMA * self.Q[new_state, :].max())
+        #print("change q[",state,"=>",new_state, "] from ", f, "=>", self.Q[state, action])
 
     def save(self, name):
+        np.savetxt(name, self.Q)
         return
     def load(self, name):
+        self.Q = np.loadtxt(name)
         return
 
 def train(agent, env, num_episodes, max_number_of_steps):
     for episode in range(num_episodes):
         observation = env.reset()
         #print(observation)
+        ok = 0
         for i in range(max_number_of_steps):
             #env.render()
             action = agent.policy(observation)
             observation2, reward,done, info = env.step(action)
             agent.update(observation, action, observation2, reward, done)
+            observation = observation2
             if done:
-                print('%d Episode finished after %f steps' % (episode, i + 1))
+                print('%d Episode finished after %d steps' % (episode, i + 1))
+                ok = 1
                 break
+        if ok==0:
+            print('%d Episode failed' %(episode))
        
 env = make_maze("space")
 agent = Agent(env)
 
 np.random.seed(0)
-num_episodes = 500
+num_episodes = 50
 max_number_of_steps = 30
 
 train(agent, env, num_episodes, max_number_of_steps)
+agent.save("q-learning")
