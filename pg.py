2974e4785abd937d8efc8e02f69ae81e08ee0708
yunfeng-net <yunfeng.net@hotmail.com>
2018-09-07 15:59:35 -0700

    seems improved
    change y_true to reflect the contribution of rewards
    special loss function to ascend by gradient

--- pg.py
+++ pg.py
@@ -3,78 +3,86 @@ import numpy as np
 import time
 import sys
 import random
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
+import keras
+from keras.layers import Input, Dense
+from keras.models import Model
 from keras import backend as K
 
-class GAgent(object):
-    def __init__(self, env):
-        self.env = env
-        self.state_size = env.observation_space.shape[0]
-        self.action_size = env.action_space.n
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
-        self.learning_rate = 0.01
-        self.batch_size = 5
-        self.model = self.build_model()
+def my_loss(y_true, y_pred):
+    return -K.mean(y_true * K.log(y_pred)) # gradient ascending
 
-    def _huber_loss(self, target, prediction):
-        # sqrt(1+error^2)-1
-        error = prediction - target
-        return K.mean(K.sqrt(1+K.square(error))-1, axis=-1)
+class GAgent(object):
+    def __init__(self, env, learning_rate=0.01, reward_decay=0.95):
+        self.env = env
+        self.action_size = env.action_space.n
+        self.state_size = env.observation_space.shape[0]
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.obs, self.pgs, self.rs, self.probs = [], [], [], []
+        self.build_model()
 
     def build_model(self):
-        H = 10
-        model = Sequential()
-        model.add(Dense(H, input_dim=self.state_size, activation='relu', kernel_initializer="glorot_normal"))
-        model.add(Dense(1, activation='sigmoid', kernel_initializer="glorot_normal"))
-        model.compile(loss='binary_crossentropy', #loss=self._huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
-        return model
-    def reshape(self, observation):
-        return np.reshape(observation, [1, self.state_size])
-    def policy(self, observation):
-        state = self.reshape(observation)
-        tfprob = self.model.predict(state)
-        return 1 if np.random.uniform() < tfprob else 0
-    def discount_rewards(r):
-        """ take 1D float array of rewards and compute discounted reward """
-        discounted_r = np.zeros_like(r)
-        running_add = 0
-        for t in reversed(xrange(0, r.size)):
-            running_add = running_add * gamma + r[t]
-            discounted_r[t] = running_add
-        discounted_epr -= np.mean(discounted_epr)
-        discounted_epr //= np.std(discounted_epr)        
-        return discounted_r
-
-    def update(self, observation, action, observation2, action2, reward, done):
-        reward = reward if not done else -10
-        self.memory.append((observation, action, observation2, action2, reward, done))
-        if done:
-            self.target_model.set_weights(self.model.get_weights())
-            return
-        if len(self.memory) <= self.batch_size:
-            return
-        minibatch = random.sample(self.memory, self.batch_size)
-        for observation, action, observation2, action2, reward, done in minibatch:
-            state = self.reshape(observation)
-            target = self.model.predict(state)
-            if done:
-                target[0][action] = reward
-            else:
-                t = self.model.predict(self.reshape(observation2))[0]
-                target[0][action] = reward + self.gamma * np.amax(t)
-            self.model.fit(state, target, epochs=1, verbose=0)
-        #print("change q[",state,"=>",new_state, "] from ", f, "=>", self.Q[state, :])
+        self.state = Input(shape=(self.state_size,))
+        #self.action = Input(shape=(self.action_size,))
+        #self.action_value = Input(shape=(1,))
+        layer1 = Dense(20, init='he_uniform',
+            #kernel_initializer=keras.initializers.RandomNormal(mean=0, stddev=0.5),
+            #bias_initializer=keras.initializers.Constant(value=0.1),
+            activation='tanh')(self.state)
+        self.action_probability = Dense(self.action_size, init='he_uniform',
+            #kernel_initializer=keras.initializers.RandomNormal(mean=0, stddev=0.5),
+            #bias_initializer=keras.initializers.Constant(value=0.1),
+            activation='softmax')(layer1)
+        self.model = Model(inputs=self.state, outputs=self.action_probability)
+        self.model.compile(optimizer=keras.optimizers.Adam(lr=self.lr),
+              #loss='categorical_crossentropy',
+              loss=my_loss,
+              metrics=['accuracy'])
 
     def load(self, name):
         self.model.load_weights(name)
-
     def save(self, name):
         self.model.save_weights(name)
+    def policy(self, observation):
+        prob = self.model.predict(observation[np.newaxis, :])
+        action = np.random.choice(range(prob.shape[1]), p=prob.ravel())
+        y = np.zeros([self.action_size])
+        y[action] = 1
+        self.pg = np.array(y).astype('float32') 
+        self.prob = prob
+        return action
+
+    def store_transition(self, s, a, r):
+        self.obs.append(s)
+        self.pgs.append(self.pg)
+        self.rs.append(r)
+        self.probs.append(self.prob)
+
+
+    def learn(self):
+        discounted_rs_norm = self._discount_and_norm_rewards()
+        a = []
+        for i in range(len(self.probs)):
+            a.append(self.pgs[i]*discounted_rs_norm[i])
+        self.model.fit(np.vstack(self.obs), np.array(a), verbose=0)
+
+        self.obs, self.ep_as, self.rs, self.probs = [], [], [], []
+        return discounted_rs_norm
+    def update(self, observation, action, observation2, action2, reward, done):
+        self.store_transition(observation, action, reward)
+        if done:
+            self.learn()
+    def _discount_and_norm_rewards(self):
+        # discount episode rewards
+        discounted_rs = np.zeros_like(self.rs)
+        running_add = 0
+        for t in reversed(range(0, len(self.rs))):
+            running_add = running_add * self.gamma + self.rs[t]
+            discounted_rs[t] = running_add
+        # normalize episode rewards
+        discounted_rs -= np.mean(discounted_rs)
+        discounted_rs /= np.std(discounted_rs)
+        return discounted_rs
 
 
 
